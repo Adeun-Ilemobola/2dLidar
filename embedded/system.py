8895a52acf183edd2d2fc1bd1a82c5b9452de89e
“Adeun” <adeunilemobola@gmail.com>
2026-01-07 23:11:01 -0800

    Refactors motor internals and implements initial lidar scanning loop.

--- embedded/system.py
+++ embedded/system.py
@@ -4,22 +4,36 @@ from gpiozero.pins.pigpio import PiGPIOFactory
 from embedded.bus import EventBus
 from embedded.modules.motors import Motor , ServoConfig
 from shared.protocol import Command, EnableMotor ,MotorAngleState
-
+from typing import List
 
 class System:
     def __init__(self, bus: EventBus) -> None:
         self.bus = bus
         self.factory = PiGPIOFactory()
+        self.isScaning = False
         self.motors = {
             "x": Motor(ServoConfig(pin=17), self.factory),
             "y": Motor(ServoConfig(pin=27), self.factory),
         }
+        self.configure_all()
 
     def configure_all(self) -> None:
         self.motors["x"].enable(True)
         self.motors["y"].enable(True)
 
         pass
+
+    def mainLoop(self) -> None:
+        if not self.isScaning:
+            return
+        AverageDistance: List[float] = []
+        while True:
+            if not self.isScaning:
+                break
+    #         collect and store 2 to 3 scans of the sensor in a for loop
+    #          then move the X coordinates by the step value of 2°
+    #           after reaching the maximum degrees for the X axis keep the last X axis, then move the Y axis
+
 
     def handle(self, cmd: Command) -> None:
         """Single entry point for UI commands."""
