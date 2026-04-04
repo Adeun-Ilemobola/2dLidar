8d107519101b20a289dd5c9cdf8cd735aed7cab8
“Adeun” <adeunilemobola@gmail.com>
2026-04-04 02:30:42 -0700

    feat: Double the range limits in sendMinMaxResult for enhanced calibration feedback

--- embedded/system.py
+++ embedded/system.py
@@ -415,10 +415,10 @@ class System:
                 
                 # Notify UI of the new "Plus/Minus" range
                 self.event_Queue.put(sendMinMaxResult(
-                   Xmin=self.limit_scam["X"]["min"],
-                   Xmax=self.limit_scam["X"]["max"],
-                   Ymin=self.limit_scam["Y"]["min"],
-                   Ymax=self.limit_scam["Y"]["max"]
+                   Xmin=self.limit_scam["X"]["min"] *2,
+                   Xmax=self.limit_scam["X"]["max"] *2,
+                   Ymin=self.limit_scam["Y"]["min"] *2,
+                   Ymax=self.limit_scam["Y"]["max"] *2
                 ))
 
                 self.event_Queue.put(CalibrationResult(
